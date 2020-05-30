e85c5141931b563cb05cda2452df24ac6a672041
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2020-05-30 21:40:51 +0300

    Update Dijkstra’s algorithm.py

--- Dijkstra’s algorithm.py
+++ Dijkstra’s algorithm.py
@@ -1,21 +1,44 @@
-#N - кількість вершин;
-#S - номер стартової вершини(з нуля);
-#matrix — матриця суміжності вихідного графа, де неіснуючі ребра мають нескінченну вагу;
-#В даному випадку нескінченність = 1000000;
-
-def Dijkstra(N, S, matrix):
-	valid = [True]*N
-	weight = [1000000]*N
-	weight[S] = 0
-	for i in range(N):
-		min_weight = 1000001
-		ID_min_weight = -1
-		for j in range(N):
-			if valid[j] and weight[j] < min_weight:
-				min_weight = weight[j]
-				ID_min_weight = j
-		for z in range(N):
-			if weight[ID_min_weight] + matrix[ID_min_weight][z] < weight[z]:
-				weight[z] = weight[ID_min_weight] + matrix[ID_min_weight][z]
-		valid[ID_min_weight] = False
-	return weight
+def dijkstra(self, node):
+        # Получает индекс узла (или поддерживает передачу int)
+        nodenum = self.get_index_from_node(node)
+        # Заставляет массив отслеживать расстояние от одного до любого узла
+        # в self.nodes. Инициализирует до бесконечности для всех узлов, кроме 
+        # начального узла, сохраняет "путь", связанный с расстоянием. 
+        # Индекс 0 = расстояние, индекс 1 = перескоки узла
+        dist = [None] * len(self.nodes)
+        for i in range(len(dist)):
+            dist[i] = [float("inf")]
+            dist[i].append([self.nodes[nodenum]])
+        
+        dist[nodenum][0] = 0
+        # Добавляет в очередь все узлы графа
+        # Отмечает целые числа в очереди, соответствующие индексам узла
+        # локаций в массиве self.nodes 
+        queue = [i for i in range(len(self.nodes))]
+        # Набор увиденных на данный момент номеров 
+        seen = set()
+        while len(queue) > 0:
+            # Получает узел в очереди, который еще не был рассмотрен
+            # и который находится на кратчайшем расстоянии от источника
+            min_dist = float("inf")
+            min_node = None
+            for n in queue: 
+                if dist[n][0] < min_dist and n not in seen:
+                    min_dist = dist[n][0]
+                    min_node = n
+            
+            # Добавляет мин. расстояние узла до увиденного, убирает очередь
+            queue.remove(min_node)
+            seen.add(min_node)
+            # Получает все следующие перескоки
+            connections = self.connections_from(min_node)
+            # Для каждой связи обновляет путь и полное расстояние от  
+            # исходного узла, если полное расстояние меньше
+            # чем текущее расстояние в массиве dist
+            for (node, weight) in connections: 
+                tot_dist = weight + min_dist
+                if tot_dist < dist[node.index][0]:
+                    dist[node.index][0] = tot_dist
+                    dist[node.index][1] = list(dist[min_node][1])
+                    dist[node.index][1].append(node)
+        return dist
